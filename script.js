25df59c6201dedf8b326ba1c38e8cbdbed63ba3a
Greg155 <gregakhun@gmail.com>
2020-10-02 09:21:38 +0200

    html partner + services

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// const services_p = document.querySelectorAll(".services__word");
+// console.log(services_p);
+
+// for(const element of services_p){
+//     element.addEventListener('mouseover', function(){
+//         element.style.color = "red";
+//     })
+// }
