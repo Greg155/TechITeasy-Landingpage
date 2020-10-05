ba8bba841ce83eb06ac2d42ea43b2fc01db799a2
walidnemri <nemriwalid@hotmail.com>
2020-10-05 11:29:43 +0200

    animation cursor circle

--- script.js
+++ script.js
@@ -7,3 +7,37 @@
 //         element.style.color = "red";
 //     })
 // }
+
+
+
+
+//console.log(cursor)
+
+// document.addEventListener('mousemove', (e) => {
+
+//     console.log(cursor.style)
+// })
+const cursor = document.getElementsByClassName('cursor')[0];
+document.onmousemove = (e) => {
+    
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+}
+
+ const h2 = document.querySelector('.subscribe h2')
+
+ console.log(h2)
+
+    h2.addEventListener('mouseover' , e => {
+        console.log('over')
+        cursor.classList.add('cursor_big')
+     })
+
+
+
+    h2.addEventListener('mouseleave' , e => {
+        console.log('leave')
+        cursor.classList.remove('cursor_big')
+     })
+
+
