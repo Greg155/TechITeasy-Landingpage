a2f75da02fa99a813d9cdd0f258c60f4e2c94985
walidnemri <nemriwalid@hotmail.com>
2020-10-06 14:53:19 +0200

    change form

--- script.js
+++ script.js
@@ -17,9 +17,55 @@
 
 //     console.log(cursor.style)
 // })
+
+let sectionHead = document.getElementsByClassName('hero');
+const bodyHeigt = document.body.clientHeight;
+console.log(sectionHead[0].clientHeight);
 const cursor = document.getElementsByClassName('cursor')[0];
+
+const elemPos =  {
+    header: document.getElementsByClassName('hero')[0].offsetTop,
+    manifesto: document.getElementsByClassName('manifesto')[0].offsetTop,
+    partnership: document.getElementsByClassName('partnership')[0].offsetTop,
+    service: document.getElementsByClassName('services__container')[0].offsetTop,
+    profile: document.getElementsByClassName('profile')[0].offsetTop,
+    subscribe: document.getElementsByClassName('subscribe')[0].offsetTop,
+    contact: document.getElementsByClassName('contact')[0].offsetTop,
+}
+console.log(elemPos)
+
+const divBottom = (elem) => {
+
+    return bodyHeigt - elem.clientHeight;
+}
+
+
+// const changeCursor = (cursorHeigth) => {
+//     let heigthToChange = 0;
+//     const valueObject = Object.values(elemPos)
+//     console.log(cursorHeigth);
+//     if (valueObject.includes(cursorHeigth)) {
+//         console.log(cursorHeigth);
+//         Object.keys(elemPos).forEach( elem => {
+//             if (elemPos[elem] === cursorHeigth) {
+//                 console.log(elem)
+//             }
+
+//     })
+
+// }
+
+// }
+
+
+
 document.onmousemove = (e) => {
     
+    console.log(e.target)
+    if (e.target.classList.value === "subhead") {
+        console.log("sub")
+    }
+    //changeCursor(e.clientY);
     cursor.style.top = `${e.clientY}px`;
     cursor.style.left = `${e.clientX}px`;
 }
